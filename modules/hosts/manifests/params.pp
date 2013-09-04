194b948119315ca29642a901a53af160b9e2235f
Thomas Böhme <thomas.boehme@devbliss.com>
2013-09-04 16:17:25 +0200

    added wheezy

--- modules/hosts/manifests/params.pp
+++ modules/hosts/manifests/params.pp
@@ -1,6 +1,6 @@
 class hosts::params {
   case $::lsbdistcodename {
-    'lenny', 'squeeze', 'maverick', 'natty', 'precise', 'raring': {
+    'lenny', 'squeeze', 'maverick', 'natty', 'precise', 'raring', 'wheezy': {
     }
     default: {
       fail("Module ${module_name} does not support ${::lsbdistcodename}")
