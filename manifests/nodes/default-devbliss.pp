59a5ee9028dc5c0b1054c9fec72ed6f06f8ff3b7
Thomas Boehme <t-lee@gmx.de>
2013-09-04 13:52:05 +0200

    disabled all the devbliss modules in default.pp and made a backup copy before

---
+++ manifests/nodes/default-devbliss.pp
@@ -0,0 +1,93 @@
+node default {
+    #################################################################
+    # create stages and bring them into correct order
+    stage { 'devbliss_base': }
+    stage { 'devbliss_advanced': }
+    stage { 'last': }
+    
+    Stage['devbliss_base']
+    -> Stage['devbliss_advanced']
+    -> Stage['main']
+    -> Stage['last']
+
+    #################################################################
+    ####  STAGE: devbliss_base
+
+    # set hostname
+    class {'hosts':    stage => devbliss_base}
+    class {'hostname': stage => devbliss_base}
+
+    class {'devbliss::locks':   stage => devbliss_base}
+    class {'ppa':               stage => devbliss_base}
+    class {'ppa::deb-devbliss': stage => devbliss_base}
+    class {'apt::update':       stage => devbliss_base}
+        
+    Class['devbliss::locks'] -> Class['ppa'] -> Class['ppa::deb-devbliss'] -> Class['apt::update']
+    
+    ####  STAGE finished: devbliss_base
+    #################################################################
+
+    #################################################################
+    ####  STAGE: devbliss_advanced
+
+    class {'backup::client':          stage => devbliss_advanced}
+    class {'nagios::target':          stage => devbliss_advanced}
+    class {'nagios::target::etc_doc': stage => devbliss_advanced}
+    class {'ganglia::client':         stage => devbliss_advanced}  
+    class {'disco_agent':             stage => devbliss_advanced}
+
+    class {'vim': stage => devbliss_advanced}
+    
+    if $fqdn == $mail_relay {
+        class {'smarthost::server': stage => devbliss_advanced}
+    } else {
+        class {'smarthost::client': stage => devbliss_advanced}
+    }
+    
+    if $is_virtual == 'false' {
+        if $is_lxc == 'false' {
+            if $fqdn != $ntp_server {
+                class {'ntp::client': stage => devbliss_advanced}
+            }
+        }
+    }
+
+    ####  STAGE finished: devbliss_advanced
+    #################################################################
+
+
+    #################################################################
+    ####  STAGE: main (this is the default stage)
+
+    class {'nfs':}
+
+    # manage sudoers
+    class {'devbliss::admin-group':}
+    class {'sudo':}
+    sudo::conf { 'admins':
+      priority => 10,
+      content  => "%admins ALL=(ALL) NOPASSWD: ALL\n",
+    }
+    Group['admins'] -> Class['sudo']
+
+    devbliss_user {[tboehme,ujanssen]: 
+        ensure  => present,
+        groups  => ['admins'],
+        require => Group['admins'],
+    }
+    devbliss_user {[njuenemann,ojohn]:
+        ensure => absent,
+    }
+
+    class {'cron':}
+
+    ####  STAGE finished: main
+    #################################################################
+
+    
+    #################################################################
+    ####  STAGE: last
+
+    ####  STAGE finished: last
+    #################################################################
+}
--- manifests/nodes/default.pp
+++ manifests/nodes/default.pp
@@ -14,15 +14,15 @@ node default {
     ####  STAGE: devbliss_base
 
     # set hostname
-    class {'hosts':    stage => devbliss_base}
-    class {'hostname': stage => devbliss_base}
+#    class {'hosts':    stage => devbliss_base}
+#    class {'hostname': stage => devbliss_base}
 
-    class {'devbliss::locks':   stage => devbliss_base}
-    class {'ppa':               stage => devbliss_base}
-    class {'ppa::deb-devbliss': stage => devbliss_base}
-    class {'apt::update':       stage => devbliss_base}
+#    class {'devbliss::locks':   stage => devbliss_base}
+#    class {'ppa':               stage => devbliss_base}
+#    class {'ppa::deb-devbliss': stage => devbliss_base}
+#    class {'apt::update':       stage => devbliss_base}
         
-    Class['devbliss::locks'] -> Class['ppa'] -> Class['ppa::deb-devbliss'] -> Class['apt::update']
+#    Class['devbliss::locks'] -> Class['ppa'] -> Class['ppa::deb-devbliss'] -> Class['apt::update']
     
     ####  STAGE finished: devbliss_base
     #################################################################
@@ -30,27 +30,25 @@ node default {
     #################################################################
     ####  STAGE: devbliss_advanced
 
-    class {'backup::client':          stage => devbliss_advanced}
-    class {'nagios::target':          stage => devbliss_advanced}
-    class {'nagios::target::etc_doc': stage => devbliss_advanced}
-    class {'ganglia::client':         stage => devbliss_advanced}  
-    class {'disco_agent':             stage => devbliss_advanced}
+#    class {'backup::client':          stage => devbliss_advanced}
+#    class {'nagios::target':          stage => devbliss_advanced}
+#    class {'ganglia::client':         stage => devbliss_advanced}  
 
-    class {'vim': stage => devbliss_advanced}
+#    class {'vim': stage => devbliss_advanced}
     
-    if $fqdn == $mail_relay {
-        class {'smarthost::server': stage => devbliss_advanced}
-    } else {
-        class {'smarthost::client': stage => devbliss_advanced}
-    }
+#    if $fqdn == $mail_relay {
+#        class {'smarthost::server': stage => devbliss_advanced}
+#    } else {
+#        class {'smarthost::client': stage => devbliss_advanced}
+#    }
     
-    if $is_virtual == 'false' {
-        if $is_lxc == 'false' {
-            if $fqdn != $ntp_server {
-                class {'ntp::client': stage => devbliss_advanced}
-            }
-        }
-    }
+#    if $is_virtual == 'false' {
+#        if $is_lxc == 'false' {
+#            if $fqdn != $ntp_server {
+#                class {'ntp::client': stage => devbliss_advanced}
+#            }
+#        }
+#    }
 
     ####  STAGE finished: devbliss_advanced
     #################################################################
@@ -59,27 +57,27 @@ node default {
     #################################################################
     ####  STAGE: main (this is the default stage)
 
-    class {'nfs':}
+#    class {'nfs':}
 
     # manage sudoers
-    class {'devbliss::admin-group':}
-    class {'sudo':}
-    sudo::conf { 'admins':
-      priority => 10,
-      content  => "%admins ALL=(ALL) NOPASSWD: ALL\n",
-    }
-    Group['admins'] -> Class['sudo']
+#    class {'devbliss::admin-group':}
+#    class {'sudo':}
+#    sudo::conf { 'admins':
+#      priority => 10,
+#      content  => "%admins ALL=(ALL) NOPASSWD: ALL\n",
+#    }
+#    Group['admins'] -> Class['sudo']
 
-    devbliss_user {[tboehme,ujanssen]: 
-        ensure  => present,
-        groups  => ['admins'],
-        require => Group['admins'],
-    }
-    devbliss_user {[njuenemann,ojohn]:
-        ensure => absent,
-    }
+#    devbliss_user {[tboehme,ujanssen]: 
+#        ensure  => present,
+#        groups  => ['admins'],
+#        require => Group['admins'],
+#    }
+#    devbliss_user {[njuenemann,ojohn]:
+#        ensure => absent,
+#    }
 
-    class {'cron':}
+#    class {'cron':}
 
     ####  STAGE finished: main
     #################################################################
