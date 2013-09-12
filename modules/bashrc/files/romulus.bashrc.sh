5f19943cbea3520fe0e28cae74f7ce8894d2e76e
Thomas Böhme <thomas.boehme@devbliss.com>
2013-09-12 11:23:58 +0200

    added VISUAL and EDITOR variables

--- modules/bashrc/files/romulus.bashrc.sh
+++ modules/bashrc/files/romulus.bashrc.sh
@@ -1,5 +1,6 @@
 alias vi='vim'
 alias l='/bin/ls -la --color=tty'
+
 HISTTIMEFORMAT='%F %T '
 export HISTTIMEFORMAT
 HISTFILESIZE=100000
@@ -7,3 +8,7 @@ export HISTFILESIZE
 HISTSIZE=100000
 export HISTSIZE
 
+VISUAL=vim
+export VISUAL
+EDITOR=vim
+export EDITOR
