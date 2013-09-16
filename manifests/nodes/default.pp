e5f47813810e8b23084aa29999341d8b9dfe2b9a
Thomas Böhme <thomas.boehme@devbliss.com>
2013-09-16 15:22:50 +0200

    enabled class cron

--- manifests/nodes/default.pp
+++ manifests/nodes/default.pp
@@ -73,7 +73,7 @@ node default {
 #        ensure => absent,
 #    }
 
-#    class {'cron':}
+    class {'cron':}
 
     ####  STAGE finished: main
     #################################################################
