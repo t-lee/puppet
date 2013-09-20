885074ec54e2e90fbdf999ffb45650dfd605e2d3
Thomas Böhme <tboehme@sVZ-PCN-287.local>
2013-09-20 10:52:55 +0200

    added file vimrc.local

--- modules/vim/manifests/init.pp
+++ modules/vim/manifests/init.pp
@@ -1,3 +1,12 @@
 class vim {
     package { "vim": ensure => latest }
+
+    file { '/etc/vim/vimrc.local':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => 644,
+        source  => "puppet://$puppetserver/modules/vim/vimrc.local",
+        require => Package['vim'],
+    }
 }
