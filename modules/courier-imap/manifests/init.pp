8c6bcc2d29528a901ec1e6ce2841c08d299edf47
Thomas Böhme <thomas.boehme@devbliss.com>
2013-10-11 13:39:13 +0200

    handle service courier-imap and courier-imap-ssl

--- modules/courier-imap/manifests/init.pp
+++ modules/courier-imap/manifests/init.pp
@@ -3,5 +3,21 @@ class courier-imap {
         ensure => installed,
     }
 
+    service { 'courier-imap':
+        ensure  => stopped,
+        enabled => true,
+        require => [Package['courier-imap-ssl'],File['/etc/courier/imapd']],
+    }
 
+    service { 'courier-imap-ssl':
+        ensure  => started,
+        enabled => true,
+        require => Package['courier-imap-ssl'],
+    }
+
+    file { '/etc/courier/imapd':
+        ensure => present,
+        source => 'puppet:///modules/courier-imap/etc/courier/imapd',
+        notify => Service['courier-imap'],
+    }
 }
