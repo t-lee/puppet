29e1fef7921a144981a3ccace75e0353b4792387
Thomas Böhme <thomas.boehme@devbliss.com>
2013-10-11 15:01:51 +0200

    remove nullmailer before installing postfix

--- modules/smarthost/manifests/init.pp
+++ modules/smarthost/manifests/init.pp
@@ -5,18 +5,21 @@ class smarthost {
     path => ["/etc/init.d", "/usr/bin"],
     onlyif => "test -f /etc/init.d/sendmail",
   }
-  package { ["sendmail", "sendmail-cf", "sendmail-base", "sendmail-bin", "sendmail-doc"]:
-    ensure => purged,
-    require => Exec["stop_sendmail"],
+  exec { "stop_nullmailer":
+    command => "/etc/init.d/nullmailer stop",
+    path => ["/etc/init.d", "/usr/bin", "/usr/sbin", "/bin", "/sbin"],
+    onlyif => "test -f /etc/init.d/nullmailer",
+  }
+  package { ["sendmail", "sendmail-cf", "sendmail-base", "sendmail-bin", "sendmail-doc", "nullmailer"]:
+    ensure  => purged,
+    require => Exec["stop_sendmail","stop_nullmailer"],
+    before  => Package['postfix'],
   }
 
   package { "postfix": 
     ensure => present,
-    require => Exec["stop_sendmail"],
   }
     
-  # Ensure that the postfix service is installed and running.
-  # Restart the service if main.cf changes.
   service { "postfix":
     enable  => "true",
     ensure  => "running",
@@ -25,66 +28,19 @@ class smarthost {
   
   # Rerun newaliases and restart postfix if aliases updated.
   exec { newaliases: 
-    command => "/usr/bin/newaliases ; service postfix restart",
-    subscribe => File["/etc/aliases"],
-    notify => Service["postfix"],
+    command     => "/usr/bin/newaliases",
   	refreshonly => true,
+  	path        => ['/usr/bin','/usr/sbin'], 
+    subscribe   => File["/etc/aliases"],
+    notify      => Service["postfix"],
   }
         
   file { "/etc/aliases":
     owner => "root",
     group => "root",
     mode => 0644,
-    source => "puppet://$puppetserver/$environment/modules/smarthost/files/aliases",
+    source => "puppet:///modules/smarthost/aliases",
     require => Package["postfix"],
-    notify  => Service["postfix"];
-  }
-
-  class client {
-    include smarthost
-    
-    file { "/etc/postfix/main.cf":
-      owner => "root",
-      group => "root",
-      mode => 0644,
-      content => template("smarthost/main.cf.erb"),
-      require => Package["postfix"],
-      notify  => Service["postfix"];
-    }
-  }
-
-  class server {
-    include smarthost
-    file { "/etc/postfix/main.cf":
-      owner => "root",
-      group => "root",
-      mode => 0644,
-      source => "puppet://$puppetserver/$environment/modules/smarthost/files/main.cf",
-      require => Package["postfix"],
-      notify  => Service["postfix"];
-    }
-    file { "/etc/postfix/virtual":
-      owner => "root",
-      group => "root",
-      mode => 0644,
-      source => "puppet://$puppetserver/$environment/modules/smarthost/files/virtual",
-    }
-    exec { "postmap virtual":
-      subscribe   => File["/etc/postfix/virtual"],
-      refreshonly => true,
-      path => ["/usr/bin", "/usr/sbin"]
-    }
-    file { "/etc/postfix/generic":
-      owner => "root",
-      group => "root",
-      mode => 0644,
-      source => "puppet://$puppetserver/$environment/modules/smarthost/files/generic",
-    }
-    exec { "postmap generic":
-      subscribe   => File["/etc/postfix/generic"],
-      refreshonly => true,
-      path => ["/usr/bin", "/usr/sbin"]
-    }
   }
 }
 
