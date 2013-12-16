7389e76748cc86941e9a641a354bae6757a0902a
Thomas Boehme <t-lee@gmx.de>
2013-12-16 20:01:27 +0100

    - renamed file from tftp.pp to tftpd.pp - added directory /var/lib/tftpboot

--- modules/pxe/manifests/tftpd.pp
+++ modules/pxe/manifests/tftpd.pp
@@ -8,14 +8,22 @@ class pxe::tftpd {
         owner   => 'root',
         group   => 'root',
         mode    => 644,
-        source  => "puppet://$puppetserver/modules/dhcpd/etc/default/tftpd-hpa",
+        source  => "puppet://$puppetserver/modules/pxe/etc/default/tftpd-hpa",
         before => Package['tftpd-hpa'],
         notify  => Service['tftpd-hpa'],
+    }
+
+    file { '/var/lib/tftpboot':
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => 755,
     }
 
     service {'tftpd-hpa':
         ensure => running,
         enable => true,
         require => Package['tftpd-hpa'],
+        require => File['/var/lib/tftpboot'],
     }
 }
