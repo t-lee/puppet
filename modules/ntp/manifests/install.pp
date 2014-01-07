403e9a5f0d7aeda72825dc57de83bd93c5e863b2
Tommy <t-lee@gmx.de>
2014-01-07 22:23:58 +0100

    removed lxc

--- modules/ntp/manifests/install.pp
+++ modules/ntp/manifests/install.pp
@@ -1,12 +1,10 @@
 class ntp::install {
   if $is_virtual == 'false' {
-    if $is_lxc == 'false' {
-      if ! defined(Package['ntp']) {
-        package { 'ntp':  ensure => installed }
-      }
-      if ! defined(Package['ntpdate']) {
-        package { 'ntpdate':  ensure => installed }
-      }
+    if ! defined(Package['ntp']) {
+      package { 'ntp':  ensure => installed }
+    }
+    if ! defined(Package['ntpdate']) {
+      package { 'ntpdate':  ensure => installed }
     }
   }
 }
--- modules/ntp/manifests/service.pp
+++ modules/ntp/manifests/service.pp
@@ -5,19 +5,11 @@ class ntp::service {
       enable => false,
     }
   }
-  if $is_lxc == 'true' {
-    service {'ntp':
-      ensure => stopped,
-      enable => false,
-    }
-  }
-  else {
-    service { 'ntp':
-      ensure     => running,
-      hasstatus  => true,
-      hasrestart => true,
-      enable     => true,
-      require    => Class['ntp::install'],
-    }
+  service { 'ntp':
+    ensure     => running,
+    hasstatus  => true,
+    hasrestart => true,
+    enable     => true,
+    require    => Class['ntp::install'],
   }
 }
