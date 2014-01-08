3f0b18f7d26084f25e1e5b9279695d60177a3c06
Tommy <t-lee@gmx.de>
2014-01-08 10:53:07 +0100

    differ between debian and ubuntu

--- modules/nfs/manifests/init.pp
+++ modules/nfs/manifests/init.pp
@@ -1,7 +1,17 @@
 class nfs {
   package { "nfs-common": ensure => latest }
 
-  service { "statd": 
+  case $::operatingsystem {
+    'Debian': {
+      $nfs_common_service_name = 'nfs-common'
+    }
+    'Ubuntu': {
+      $nfs_common_service_name = 'statd'
+    }
+    default:  {fail('not supported OS')}
+  }
+
+  service { "$nfs_common_service_name": 
     enable     => true,
     ensure     => running,
     require => Package[nfs-common],
