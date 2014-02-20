0193a95906081a8de4be96ea7a850c11bbec1b77
tommy <t-lee@gmx.de>
2014-02-20 23:17:12 +0100

    defined settings for host sha-ka-ree in pxe::dhcpd class

--- manifests/nodes/sha-ka-ree.neutral-zone.de.pp
+++ manifests/nodes/sha-ka-ree.neutral-zone.de.pp
@@ -6,5 +6,21 @@ node 'sha-ka-ree.neutral-zone.de' inherits default {
     class {'fetchmail':}
     class {'bind':}
     class {'pxe':}
+    class {'pxe::dhcpd':
+        domain_name         => "neutral-zone.de",
+        domain_name_servers => "192.168.0.1",
+        subnet_mask         => "255.255.255.0",
+        server_name         => "sha-ka-ree",
+        next_server         => "192.168.0.4",
+        subnet              => "192.168.0.0",
+        netmask             => "255.255.255.0",
+        range_start         => "192.168.0.100",
+        range_end           => "192.168.0.199",
+        routers             => "192.168.0.1" 
+    }
+    nfs::export {'/var/lib/tftpboot/install':
+        host    => '192.168.0.0/24',
+        options => 'ro,async,no_root_squash,no_subtree_check' 
+    }
     nfs::export {'/media/data/musik': host => '172.18.10.5'}
 }
