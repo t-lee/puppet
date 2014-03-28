b69bae71628b2b9fff429af5bde9ca875449cef3
Tommy <t-lee@gmx.de>
2014-03-28 22:21:40 +0100

    changed names

--- modules/backup/manifests/client.pp
+++ modules/backup/manifests/client.pp
@@ -10,14 +10,14 @@
 #   class {'backup::client':}
 #
 class backup::client {
-    ssh_authorized_key { backup_client:
+    ssh_authorized_key { backup_client_sha_ka_ree:
         ensure  => present,
         name    => "root@sha-ka-ree",
         type    => "ssh-rsa",
         key     => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDgKSDtTWv3jfiTMRC+DO7BVFQ6olg7fBygpKOzrQSxhCfoLmbMhuWqTf7fhU6nAsY3uIjeKlAncWtT/dS8VFzqmgjnVYie4JES8K9JzOH9suYBBqX/Jt/KFDE0sIMKMh1yltT5I5GcSibzMgdN9/F1tTyXAbss46oplVcWhQB/hSiYqS76Rc5qnZm50W4AKZeH0Uaot8zD9FB3P4jHvMPaeCrwtGaLiJ3rK+a+V75btk3H/ps4OuiGnqlOl4wQgE14+mxd7OIXUJ0Mb2e16J4wcszg7bXbQ9WK4+00K5SudNcBGFn32AxKZldEBHuiX32dxCY9RvnOMR9ps+xHrKhv",
         user    => root,
     }
-    ssh_authorized_key { backup_client:
+    ssh_authorized_key { backup_client_romulus:
         ensure  => present,
         name    => "root@romulus",
         type    => "ssh-dss",
