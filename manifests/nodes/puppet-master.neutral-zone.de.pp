3c191cd13ce8347c1606eb3180b27f4922dab62b
root <root@puppet-master>
2017-03-25 21:35:42 +0100

    adds manifest file for node puppet-master

---
+++ manifests/nodes/puppet-master.neutral-zone.de.pp
@@ -0,0 +1,25 @@
+node 'puppet-master.neutral-zone.de' inherits default2 {
+    #class {'openvpn':}
+    #class {'backup::server':}
+    #class {'autofs':}
+    #class {'courier-imap':}
+    #class {'fetchmail':}
+    #class {'bind':}
+    #class {'pxe':}
+    #class {'pxe::dhcpd':
+    #    domain_name         => "neutral-zone.de",
+    #    domain_name_servers => "192.168.0.1",
+    #    subnet_mask         => "255.255.255.0",
+    #    server_name         => "puppet-test",
+    #    next_server         => "192.168.111.1",
+    #    subnet              => "192.168.111.0",
+    #    netmask             => "255.255.255.0",
+    #    range_start         => "192.168.111.100",
+    #    range_end           => "192.168.111.199",
+    #    routers             => "192.168.111.1" 
+    #}
+    #nfs::export {'/var/lib/tftpboot/install':
+    #    host    => '192.168.111.0/24',
+    #    options => 'ro,async,no_root_squash,no_subtree_check' 
+    #}
+}
