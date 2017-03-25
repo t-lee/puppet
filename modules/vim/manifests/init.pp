24c212e87fc154c0f8e49b5d0773728a76d4003c
root <root@puppet-master>
2017-03-25 21:36:43 +0100

    fixes file mode syntax, uses set editor alternative link to vim

--- modules/vim/manifests/init.pp
+++ modules/vim/manifests/init.pp
@@ -5,8 +5,14 @@ class vim {
         ensure  => present,
         owner   => 'root',
         group   => 'root',
-        mode    => 644,
+        mode    => '644',
         source  => "puppet:///modules/vim/vimrc.local",
+        require => Package['vim'],
+    }
+
+    # Will also update gem, irb, rdoc, rake, etc.
+    alternatives { 'editor':
+        path    => '/usr/bin/vim.basic',
         require => Package['vim'],
     }
 }
